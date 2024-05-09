2a119eb8bd477c504b59448ed73a09b756b01117
Tom Hubrecht <tom@hubrecht.ovh>
2024-05-09 21:05:04 +0200

    feat(loadcredential): Initial code

---
+++ default.nix
@@ -0,0 +1,40 @@
+{
+  sources ? import ./npins,
+  pkgs ? import sources.nixpkgs { },
+}:
+
+let
+  deploy-pypi = pkgs.writeShellApplication {
+    name = "deploy-pypi";
+
+    runtimeInputs = [
+      (pkgs.python3.withPackages (ps: [
+        ps.setuptools
+        ps.build
+        ps.twine
+      ]))
+    ];
+
+    text = ''
+      # Clean the repository
+      rm -r dist
+
+      python -m build
+      twine upload dist/*
+    '';
+  };
+in
+
+{
+  devShell = pkgs.mkShell {
+    name = "loadcredential.dev";
+
+    packages = [ ];
+  };
+
+  publishShell = pkgs.mkShell {
+    name = "loadcredential.publish";
+
+    packages = [ deploy-pypi ];
+  };
+}
